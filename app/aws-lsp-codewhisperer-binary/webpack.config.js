1b21c4b76525fc36ad0c21d10c6ff2c6558f35e3
Kenneth Ma <16969131+makenneth@users.noreply.github.com>
2024-05-17 09:17:43 -0700

    update bundle to include chat server and wasm files

--- app/aws-lsp-codewhisperer-binary/webpack.config.js
+++ app/aws-lsp-codewhisperer-binary/webpack.config.js
@@ -10,6 +10,9 @@ const baseConfig = {
             type: 'umd',
         },
     },
+    experiments: {
+        asyncWebAssembly: true,
+    },
     resolve: {
         extensions: ['.ts', '.tsx', '.js'],
     },
@@ -28,6 +31,13 @@ const nodeJsBundleConfig = {
     ...baseConfig,
     entry: {
         'aws-lsp-codewhisperer-binary': path.join(__dirname, 'src/index.ts'),
+        fqnExtractorWorker: '@aws/lsp-fqn/out/node/fqnExtractorWorker.js',
+    },
+    resolve: {
+        ...baseConfig.resolve,
+        alias: {
+            '@aws/fully-qualified-names$': '@aws/fully-qualified-names/node/aws_fully_qualified_names.js',
+        },
     },
     target: 'node',
 }
